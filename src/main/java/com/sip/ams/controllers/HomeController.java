4ad91e731c4bdf6948a940a8a15f2409d982461e
Athimni.afrah <athimni.afrah@gmail.com>
2022-02-06 16:37:02 +0100

    css

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -3,12 +3,21 @@ package com.sip.ams.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/info")
 	public String info(Model model) {
+		ArrayList<String> ls = new ArrayList<String>();
+	    ls.add("OCA");	
+	    ls.add("OCP");
+	    ls.add("Spring");
+	    ls.add("Angular");
+	    model.addAttribute("ls", ls);
+
+	    
 		
 		String formation = "fullstack";
 		model.addAttribute("Workshop", formation);
